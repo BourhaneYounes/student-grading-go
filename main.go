3ac2481fef694414b5f358c847ea45a97656b751
BourhaneYounes <bourhaneyounes1@gmail.com>
2023-03-04 15:04:55 +0000

    added parsing csv

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	_ "io"
+	"os"
+	"strconv"
+	"strings"
+)
 type Grade string
 
 const (
@@ -20,8 +28,39 @@ type studentStat struct {
 	grade      Grade
 }
 
+func check(e error){
+	if e!= nil {
+		panic(e)
+	}
+}
+func convert(str string) int{
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
+
 func parseCSV(filePath string) []student {
-	return nil
+	students := make([]student, 0)
+	file, err := os.Open(filePath)
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	for scanner.Scan(){
+		line := strings.Split(scanner.Text(), ",")
+		students = append(students, student{firstName: line[0],
+							lastName: line[1],	
+							university: line[2],	
+							test1Score: convert(line[3]),
+							test2Score: convert(line[4]),
+							test3Score: convert(line[5]),
+							test4Score: convert(line[6]),
+		})
+	}
+
+	return students 
 }
 
 func calculateGrade(students []student) []studentStat {
@@ -34,4 +73,8 @@ func findOverallTopper(gradedStudents []studentStat) studentStat {
 
 func findTopperPerUniversity(gs []studentStat) map[string]studentStat {
 	return nil
+}
+
+func main(){
+	fmt.Println(parseCSV("./grades.csv"))
 }
